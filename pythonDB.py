b2b5c6b17df01d425f4addddd74fdaa37740f664
Lirodek <96456897+Lirodek@users.noreply.github.com>
2022-10-16 13:18:06 +0900

    change
    chageBranch

---
+++ pythonDB.py
@@ -0,0 +1,94 @@
+# STEP 1
+import pymysql
+
+# STEP 2: MySQL Connection 연결
+# 한글처리 (charset = 'utf8')
+
+# ===== default =====
+cur         = None
+con         = None
+# ===================
+
+
+def open():
+    global cur, con
+    # pythondb.ctozk8eaqm8x.ap-northeast-2.rds.amazonaws.com
+    con = pymysql.connect(host='127.0.0.1', user='python', password='3302', db='pythonDB', charset='utf8') 
+    cur = con.cursor() 
+
+def test(TEST):
+    open()
+    global con, cur
+    sql = """	
+        SELECT 
+		  A.timer_hour		
+		, A.timer_minute
+		, A.LNK
+		FROM (
+			SELECT  timer_hour
+                    , timer_minute
+                    , LNK
+                FROM selectday 
+                WHERE USER = 'Lirodek'
+                AND TIMER_DATE = DATE_FORMAT(now(), '%Y%m%d')
+            UNION 
+            SELECT timer_hour
+                    , timer_minute 
+                    , LNK
+                FROM loof
+                WHERE USER = 'lirodek'
+                AND wed = 1
+            ) A
+            WHERE CONCAT(A.timer_hour , A.TIMER_MINUTE) >= CONCAT(DATE_FORMAT(NOW(), '%H%i'))
+            AND TIMER_HOUR >= HOUR(NOW())
+            ORDER BY CONCAT(A.timer_hour , A.TIMER_MINUTE) desc
+			LIMIT 1
+        """
+
+    cur.execute(sql) 
+    res = cur.fetchall() 
+    print(res)
+    close()
+    return res
+
+def insertLoofTimer():
+    global cur, con
+    open()
+
+    sql = """
+        INSERT 
+    """
+
+    cur.ex
+
+
+    
+    return 
+    
+
+def close():
+    global con
+    con.commit() 
+    con.close() 
+# class Database:
+#     def openDB():
+#         global cur
+#         global con
+#         # 데이터베이스 연결
+#         con = pymysql.connect(host='localhost', user='python', password='3302', db='pythonDB', charset='utf8') 
+    
+#         # STEP 3: Connection 으로부터 Cursor 생성
+#         cur = con.cursor()
+        
+
+#     # 반복되는 알람
+#     def insert_recurring_alarm():
+        
+#         # SQL문 실행 및 Fetch
+#         sql = "SELECT * FROM timer"
+#         cur.execute(sql)
+#         result = cur.fetchall()
+#         print(result)
+#         con.close()
+#         # 데이타 Fetch return
+    
--- timer.py
+++ timer.py
@@ -4,8 +4,13 @@
 
 ### 3. 유튜브 매크로 알람 시계
 
+<<<<<<< Updated upstream
 ### 디지털 시계 코드
 # 배광민 첫번째 주석 1 
+=======
+### 디지털 시계 코드 
+from calendar import week
+>>>>>>> Stashed changes
 import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -104,6 +109,7 @@ class CWidget(QWidget):
 
         # 자정에 매크로 초기화
         if kor.tm_hour == 0 and kor.tm_min == 0 and kor.tm_hour == 0 and kor.tm_sec == 0 :
+            self.updateData()
             macro = True
 
 
@@ -111,6 +117,64 @@ class CWidget(QWidget):
         timer = Timer(1, self.showtime)
         timer.start()
 
+<<<<<<< Updated upstream
+=======
+    def updateData(self):
+        global hour, minute, link
+        result = self.rows( daillyDay[ datetime.today().weekday() ])
+        if len(result) == 0 :
+            QMessageBox. question(self, '확인창', '알람설정하시겠어요?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        for tuple in result:
+            hour = tuple[0]
+            minute = tuple[1]
+            link = tuple[2]
+            print(tuple[0])
+            
+
+    # 시, 분을 가져오는 함수
+    def onActivated(self, text):
+        temp = ""
+        for char in range(len(text)-1):
+            if type(text[char]) != "<class 'str'>" :
+                temp+= text[char]
+        if text[len(text)-1] == "분":
+            global temp_minute
+            temp_minute = int(temp)
+        if text[len(text)-1] == "시":
+            global temp_hour
+            temp_hour = int(temp)
+
+    # 요일 버튼을 리셋해주는 함수
+    def weekButtonReset(self):
+        i = 0
+        for day in self.days:
+            day.setStyleSheet(defaultButtonString)
+            week_hover[i] = 0
+            i+=1
+
+    def applyBtn_event(self):
+        global temp_hour
+        global temp_minute
+        global hour
+        global minute
+        global macro
+        global link
+
+        reply = QMessageBox.question(self, '확인창', '알람설정하시겠어요?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            macro = True
+            self.weekButtonReset()
+
+            if self.editText.text() != "":
+                link = self.editText.text()
+
+            print('알람 저장되어야할 값들', temp_hour,'시', temp_minute, week_hover)
+            hour = temp_hour
+            minute = temp_minute
+
+        else:
+            print('취소되었습니다.')
+>>>>>>> Stashed changes
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
