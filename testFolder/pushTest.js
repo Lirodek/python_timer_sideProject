db30eb41c2ff9dac12ce7edbdb636044c1b113a7
gyoseok <dhkk65@gmail.com>
2023-08-13 20:05:52 +0900

    주석 추가

--- testFolder/pushTest.js
+++ testFolder/pushTest.js
@@ -12,16 +12,16 @@ document.addEventListener('keydown', function(event) {
 		  minute *= 60;
 		  hour *= 3600;
 		  second = second + minute + hour;
-	  } else {
+	  } else { 
 		  minute = parseInt(checkTimer[0]);
 		  second = parseInt(checkTimer[1]);
 		  minute *= 60;
 		  second = second + minute;
 	  }
-	  second = second+4;
+	  second = second+4; //앞으로 ?초 만큼(현재 4초)
 	  playBookmark(second);
 	}
-  if (event.keyCode === 37) { // 39 is the key code for the right arrow key
+  if (event.keyCode === 37) { 
 	  let checkTimer = document.getElementsByClassName('fp-elapsed')[0].textContent;
 	  let hour = 0;
 	  let minute = 0;
@@ -40,9 +40,8 @@ document.addEventListener('keydown', function(event) {
 		  minute *= 60;
 		  second = second + minute;
 	  }
-	  second = second-4;
+	  second = second-4; //뒤로 ?초 만큼(현재 4초)
 	  playBookmark(second);
 	}
 	  
   });
-
