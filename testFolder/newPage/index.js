4341538ec75d660eec1585184810db4566941dbf
gyoseok <dhkk65@gmail.com>
2023-08-20 19:37:01 +0900

    아몰랑 커밋

--- testFolder/newPage/index.js
+++ testFolder/newPage/index.js
@@ -1,30 +1,34 @@
 document.addEventListener('keydown', function(event) {
-  if (event.keyCode === 39) { // 39 is the key code for the right arrow key
-	let checkTimer = document.getElementsByClassName('fp-elapsed')[0].textContent;
-	let hour = 0;
-	let minute = 0;
-	let second = 0;
-	checkTimer = checkTimer.split(':');
-	if(checkTimer.length == 3){
-		console.log('여기실행됨');
-		hour = parseInt(checkTimer[0]);
-		minute = parseInt(checkTimer[1]);
-		second = parseInt(checkTimer[2]);
-		minute *= 60;
-		hour *= 3600;
-		second = second + minute + hour;
-	} else {
-		minute = parseInt(checkTimer[0]);
-		second = parseInt(checkTimer[1]);
-		minute *= 60;
-		second = second + minute;
-	}
-	second = second+5;
+	console.log('ㅎㅇ');
 	try{
+		if (event.keyCode === 39) { // 39 is the key code for the right arrow key
+		let checkTimer = document.getElementsByClassName('fp-elapsed')[0].textContent;
+		let hour = 0;
+		let minute = 0;
+		let second = 0;
+		checkTimer = checkTimer.split(':');
+		if(checkTimer.length == 3){
+			console.log('여기실행됨');
+			hour = parseInt(checkTimer[0]);
+			minute = parseInt(checkTimer[1]);
+			second = parseInt(checkTimer[2]);
+			minute *= 60;
+			hour *= 3600;
+			second = second + minute + hour;
+		} else {
+			minute = parseInt(checkTimer[0]);
+			second = parseInt(checkTimer[1]);
+			minute *= 60;
+			second = second + minute;
+		}
+		second = second+5;
+	
+		console.log('통과');
 		playBookmark(second);
-	}catch{
-		
+		}
+	} catch (error) {
+		console.log('불통과', error);
 	}
     
-  }
+  
 });
